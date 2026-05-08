596e813e9c8a863272f9d013484ac74a7e15d435
Bennet Yee <bennet.yee@gmail.com>
2026-05-08 00:03:30 -0700

    options bugs. allow id vs name. notifier program and custom string.

--- breaker_monitor.py
+++ breaker_monitor.py
@@ -12,22 +12,38 @@ MIN_CHECK_INTERVAL = 1
 MIN_POWER = 0.1
 MIN_MIN_POWER = 0.001
 CIRCUIT_NAME='Kitchen / Oven'
+NOTIFIER_PROGRAM='alert'
+# An annoying / loud notification.  I use this script:
+# (zenity --info --text="$*" --timeout=600; echo quit) | while ! read -t 1; do espeak -v 'en-us' "$*"; done
 
-def circuit_has_power(name, min_power):
-    return abs(span_panel.circuit_power_by_name(name)) > MIN_POWER
+def circuit_has_power(id, name, min_power):
+    if id != '':
+        power = span_panel.circuit_power_by_id(id)
+    else:
+        power = span_panel.circuit_power_by_name(name)
+    if power is None:
+        sys.stderr.write(f'circuit id="{id}", name="{name}" does not exist/match\n')
+        sys.exit(1)
+    return abs(power) > min_power
 
 def main(argv):
     p = argparse.ArgumentParser()
     p.add_argument('--show-progress', type=bool, default=False,
                    action=argparse.BooleanOptionalAction,
-                   help='print dots and commas to show progress')
+                   help='Show polling activity.')
     p.add_argument('--check-interval', type=int, 
                    default=CHECK_INTERVAL_SEC,
-                   help='number of seconds between polling')
+                   help='Polling interval, in seconds')
     p.add_argument('--min-power', type=float, default=MIN_POWER,
-                   help='power level below which the circuit is considered to be off')
+                   help='Power level below which the circuit is considered to be off')
     p.add_argument('--circuit', type=str, default=CIRCUIT_NAME,
-                   help='name of the circuit to monitor')
+                   help='Name of the circuit to monitor.  If --id is specified, this is ignored.')
+    p.add_argument('--id', type=str, default='',
+                   help='Id of the circuit to monitor')
+    p.add_argument('--notifier', type=str, default=NOTIFIER_PROGRAM,
+                   help='Name of notification / alert program')
+    p.add_argument('--message', type=str, default=None,
+                   help='Argument for notification program')
 
     options = p.parse_args(argv[1:])
 
@@ -42,9 +58,15 @@ def main(argv):
 
     dots = 0
     while True:
-        if not circuit_has_power(options.circuit, options.min_power):
-            subprocess.run(['alert', 'check', 'breaker', 'for', options.circuit])
-        time.sleep(CHECK_INTERVAL_SEC)
+        if not circuit_has_power(options.id, options.circuit, options.min_power):
+            message = options.message
+            if message is None:
+                if options.id != '':
+                    message = f'Check the circuit breaker'
+                else:
+                    message = f'Check the {options.circuit} circuit breaker'
+            subprocess.run([options.notifier, message])
+        time.sleep(options.check_interval)
         if options.show_progress:
             dots += 1
             if dots % 100 == 0:
